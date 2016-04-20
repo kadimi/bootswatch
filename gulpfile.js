bbdcea28a4b31e9e2c0e34dcc1959188ec5d8e91
Nabil Kadimi <nabil@kadimi.com>
2016-04-21 00:37:36 +0100

    Beatify JavaScript code

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,63 @@
 /* globals require */
 
-var del = require('del');
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var merge = require('merge-stream');
-var remoteSrc = require('gulp-remote-src');
-var rename = require('gulp-rename');
-var unzip = require('gulp-unzip');
+var i;
+var del = require( 'del' );
+var gulp = require( 'gulp' );
+var jscs = require( 'gulp-jscs' );
+var merge = require( 'merge-stream' );
+var remoteSrc = require( 'gulp-remote-src' );
+var rename = require( 'gulp-rename' );
+var unzip = require( 'gulp-unzip' );
 
 var github = 'https://github.com/';
 var githubUC = 'https://raw.githubusercontent.com/';
 
 // Run all tasks.
-gulp.task('default', ['clean'], function() {
-	gulp.start('bootstrap.get', 'bootswatch.get', 'less.php', 'tgmpa.get', '.jshintrc.get');
+gulp.task( 'default', ['clean'], function() {
+	gulp.start( 'bootstrap.get', 'bootswatch.get', 'less.php', 'tgmpa.get' );
 });
 
-// Delete vendor folder 
-gulp.task('clean', function(cb) {
-	return del('vendor/**',cb);
+// Delete vendor folder
+gulp.task( 'clean', function( cb ) {
+	return del( 'vendor/**', cb );
 });
 
 // Downloads TGMPA class.
-gulp.task('tgmpa.get', function() {
+gulp.task( 'tgmpa.get', function() {
 	var tgmpaBase = githubUC + 'TGMPA/TGM-Plugin-Activation/master/';
-	return remoteSrc('class-tgm-plugin-activation.php', {base: tgmpaBase})
-		.pipe(gulp.dest('vendor/TGM-Plugin-Activation'))
+	return remoteSrc( 'class-tgm-plugin-activation.php', { base: tgmpaBase })
+		.pipe( gulp.dest( 'vendor/TGM-Plugin-Activation' ) )
 	;
 });
 
 // Download Bootstrap JavaScript file.
-gulp.task('bootstrap.get', function() {
+gulp.task( 'bootstrap.get', function() {
 	var tasks = [];
 	var bsBase = githubUC + 'twbs/bootstrap/master/';
 	var bsFonts = ['glyphicons-halflings-regular.eot', 'glyphicons-halflings-regular.svg', 'glyphicons-halflings-regular.ttf', 'glyphicons-halflings-regular.woff', 'glyphicons-halflings-regular.woff2'];
 
-	var css = remoteSrc('css/bootstrap.min.css', {base: bsBase + 'dist/'})
-		.pipe(gulp.dest('vendor/bootstrap'))
+	var css = remoteSrc( 'css/bootstrap.min.css', { base: bsBase + 'dist/' })
+		.pipe( gulp.dest( 'vendor/bootstrap' ) )
 	;
-	var js = remoteSrc('js/bootstrap.min.js', {base: bsBase + 'dist/'})
-		.pipe(gulp.dest('vendor/bootstrap'))
+	var js = remoteSrc( 'js/bootstrap.min.js', { base: bsBase + 'dist/' })
+		.pipe( gulp.dest( 'vendor/bootstrap' ) )
 	;
-	var variables = remoteSrc('less/variables.less', {base: bsBase})
-		.pipe(gulp.dest('vendor/bootstrap'))
+	var variables = remoteSrc( 'less/variables.less', { base: bsBase })
+		.pipe( gulp.dest( 'vendor/bootstrap' ) )
 	;
 
 	// Fonts
-	for (var i = 0; i < bsFonts.length; i++) {
-		tasks[i] = remoteSrc('/fonts/' + bsFonts[i], {base: bsBase + '/'})
-			.pipe(gulp.dest('vendor/bootstrap'))
+	for ( i = 0; i < bsFonts.length; i++ ) {
+		tasks[i] = remoteSrc( '/fonts/' + bsFonts[i], { base: bsBase + '/' })
+			.pipe( gulp.dest( 'vendor/bootstrap' ) )
 		;
 	}
 
-	return merge(css, js, variables, tasks);
+	return merge( css, js, variables, tasks );
 });
 
 // Download bootswatch themes, variables and fonts.
-gulp.task('bootswatch.get', function() {
+gulp.task( 'bootswatch.get', function() {
 	var tasks = [];
 	var bwThemes = ['cerulean', 'cosmo', 'cyborg', 'darkly', 'flatly', 'journal', 'lumen', 'paper', 'readable', 'sandstone', 'simplex', 'slate', 'spacelab', 'superhero', 'united', 'yeti'];
 	var bwFonts = ['glyphicons-halflings-regular.eot', 'glyphicons-halflings-regular.svg', 'glyphicons-halflings-regular.ttf', 'glyphicons-halflings-regular.woff', 'glyphicons-halflings-regular.woff2'];
@@ -64,61 +65,56 @@ gulp.task('bootswatch.get', function() {
 	var i;
 
 	// Themes
-	for (i = 0; i < bwThemes.length; i++) {
-		tasks[i] = remoteSrc('bootstrap.min.css', {base: bwBase + bwThemes[i] + '/'})
-			.pipe(rename('theme.min.css'))
-			.pipe(gulp.dest('vendor/bootswatch/themes/' + bwThemes[i]))
+	for ( i = 0; i < bwThemes.length; i++ ) {
+		tasks[i] = remoteSrc( 'bootstrap.min.css', { base: bwBase + bwThemes[i] + '/' })
+			.pipe( rename( 'theme.min.css' ) )
+			.pipe( gulp.dest( 'vendor/bootswatch/themes/' + bwThemes[i] ) )
 		;
 	}
 
 	// Variables
-	for (i = 0; i < bwThemes.length; i++) {
-		tasks[i] = remoteSrc('variables.less', {base: bwBase + bwThemes[i] + '/'})
-			.pipe(rename('vars.less'))
-			.pipe(gulp.dest('vendor/bootswatch/themes/' + bwThemes[i]))
+	for ( i = 0; i < bwThemes.length; i++ ) {
+		tasks[i] = remoteSrc( 'variables.less', { base: bwBase + bwThemes[i] + '/' })
+			.pipe( rename( 'vars.less' ) )
+			.pipe( gulp.dest( 'vendor/bootswatch/themes/' + bwThemes[i] ) )
 		;
 	}
 
 	// Fonts
-	for (i = 0; i < bwFonts.length; i++) {
-		tasks[i] = remoteSrc('fonts/' + bwFonts[i], {base: bwBase + '/'})
-			.pipe(gulp.dest('vendor/bootswatch'))
+	for ( i = 0; i < bwFonts.length; i++ ) {
+		tasks[i] = remoteSrc( 'fonts/' + bwFonts[i], { base: bwBase + '/' })
+			.pipe( gulp.dest( 'vendor/bootswatch' ) )
 		;
 	}
 
-	return merge(tasks);
+	return merge( tasks );
 });
 
 // Download oyejorge/less.php from github
-gulp.task('less.php.get', function() {
-	return remoteSrc('master.zip', {base: github + 'oyejorge/less.php/archive/'})
-		.pipe(unzip())
-		.pipe(gulp.dest('vendor'))
+gulp.task( 'less.php.get', function() {
+	return remoteSrc( 'master.zip', { base: github + 'oyejorge/less.php/archive/' })
+		.pipe( unzip() )
+		.pipe( gulp.dest( 'vendor' ) )
 	;
 });
-gulp.task('less.php.rename', ['less.php.get'], function() {
+gulp.task( 'less.php.rename', ['less.php.get'], function() {
 	return gulp
-		.src('vendor/less.php-master/**/*', {dot: true})
-		.pipe(gulp.dest('vendor/less.php'))
+		.src( 'vendor/less.php-master/**/*', { dot: true })
+		.pipe( gulp.dest( 'vendor/less.php' ) )
 	;
 });
-gulp.task('less.php.clean', ['less.php.rename'], function() {
-	return del('vendor/less.php-master');
+gulp.task( 'less.php.clean', ['less.php.rename'], function() {
+	return del( 'vendor/less.php-master' );
 });
-gulp.task('less.php', ['less.php.clean']);
+gulp.task( 'less.php', ['less.php.clean']);
 
 // JavaScript
-gulp.task('.jshintrc.clean', function() {
-	return del('.jshintrc');
-});
-gulp.task('.jshintrc.get', ['.jshintrc.clean'], function() {
-	return remoteSrc('.jshintrc', {base: 'https://develop.svn.wordpress.org/trunk/'})
-		.pipe(gulp.dest('.'))
-	;
-});
-gulp.task('jshint', ['.jshintrc.get'], function() {
+gulp.task( 'jscs', function() {
 	return gulp.src(['*.js', '!vendor/*.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
+		.pipe( jscs() )
+		.pipe( jscs({ fix: true }) )
+		.pipe( jscs.reporter() )
+		.pipe( jscs.reporter( 'fail' ) )
+		.pipe( jscs.reporter() )
 	;
 });
--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 /**
- * script.js
+ * File script.js.
  *
  * Theme main JavaScript file.
  */
-jQuery(document).ready( function($) {
+jQuery( document ).ready( function( $ ) {
 
 	// Add Bootstrap classes in some important elements.
-	$('table').addClass('table table-striped table-hover');
+	$( 'table' ).addClass( 'table table-striped table-hover' );
 
 	/**
 	 * Add caret to parents in primary menu
 	 *
 	 * @todo Remove at WordPress 4.4.
 	 */
-	$('#wp-calendar').addClass( 'table table-striped table-bordered');
-	$('.menu-item-has-children > a').append(' <span class="caret"></span>');
+	$( '#wp-calendar' ).addClass( 'table table-striped table-bordered' );
+	$( '.menu-item-has-children > a' ).append( ' <span class="caret"></span>' );
 });
