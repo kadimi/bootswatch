e6ee984870d1da5b57ee1d90aa7d3cb057875685
Nabil Kadimi <nabil@kadimi.com>
2016-04-25 00:18:30 +0100

    Customize comments form

--- inc/extras/comment-form.php
+++ inc/extras/comment-form.php
@@ -15,7 +15,6 @@ function bootswatch_comment_form_fields( $fields ) {
 
 	$commenter = wp_get_current_commenter();
 	$req = get_option( 'require_name_email' );
-	$aria_req = $req ? ' aria-required="true"' : '';
 	$html5 = current_theme_supports( 'html5', 'comment-form' ) ? true : false;
 
 	$div_tpl = '<div class="form-group comment-form-{{field}}">{{label}} {{input}}</div>';
@@ -26,31 +25,30 @@ function bootswatch_comment_form_fields( $fields ) {
 
 	$fields_parameters = array(
 		'author' => array(
-			'label'       => __( 'Name' ),
-			'type'        => 'text',
-			'value'       => esc_attr( $commenter['comment_author'] ),
-			'required'    => $req,
-			'size'        => 30,
-			'maxlength'   => 245,
-			'describedby' => '',
+			'label'         => __( 'Name' ),
+			'type'          => 'text',
+			'value'         => esc_attr( $commenter['comment_author'] ),
+			'required'      => $req ? 'required' : '',
+			'aria-required' => $req ? 'true' : '',
+			'size'          => 30,
+			'maxlength'     => 245,
 		),
 		'email' => array(
-			'label'       => __( 'Email' ),
-			'type'        => $html5 ? 'email' : 'text',
-			'value'       => esc_attr( $commenter['comment_author_email'] ),
-			'required'    => $req,
-			'size'        => 30,
-			'maxlength'   => 100,
-			'describedby' => 'email-notes',
+			'label'         => __( 'Email' ),
+			'type'          => $html5 ? 'email' : 'text',
+			'value'         => esc_attr( $commenter['comment_author_email'] ),
+			'required'      => $req ? 'required' : '',
+			'aria-required' => $req ? 'true' : '',
+			'size'          => 30,
+			'maxlength'     => 100,
+			'describedby'   => 'email-notes',
 		),
 		'url' => array(
-			'label'       => __( 'Website' ),
-			'type'        => $html5 ? 'url' : 'text',
-			'value'       => esc_attr( $commenter['comment_author_url'] ),
-			'required'    => false,
-			'size'        => 30,
-			'maxlength'   => 200,
-			'describedby' => '',
+			'label'     => __( 'Website' ),
+			'type'      => $html5 ? 'url' : 'text',
+			'value'     => esc_attr( $commenter['comment_author_url'] ),
+			'size'      => 30,
+			'maxlength' => 200,
 		),
 	);
 
@@ -61,8 +59,8 @@ function bootswatch_comment_form_fields( $fields ) {
 
 		// Build <input>.
 		$input = $input_tpl;
-		foreach ( array( 'name', 'id', 'type', 'value', 'required', 'size', 'maxlength', 'describedby', 'class' ) as $p ) {
-			if ( $parameter[ $p ] ) {
+		foreach ( array( 'name', 'id', 'type', 'value', 'required', 'aria-required', 'size', 'maxlength', 'describedby', 'class' ) as $p ) {
+			if ( ! empty($parameter[ $p ]) ) {
 				$input = str_replace( '{{param}}', sprintf( ' %s="%s"{{param}}', $p, $parameter[ $p ] ), $input );
 			}
 		}
@@ -71,11 +69,11 @@ function bootswatch_comment_form_fields( $fields ) {
 		// Build <label>.
 		$label = $label_tpl;
 		$label = str_replace( '{{label}}', $parameter['label'], $label );
-		$label = str_replace( '{{required}}', $parameter['required'] ? ' <span class="required">*</span>' : '', $label );
+		$label = str_replace( '{{required}}', !empty($parameter['required']) ? ' <span class="required">*</span>' : '', $label );
 
 		// Build field <div>.
 		$div = $div_tpl;
-		$div = str_replace( array( '{{input}}', '{{label}}', '{{field}}' ), array( $input, $label, $field ),$div );
+		$div = str_replace( array( '{{input}}', '{{label}}', '{{field}}' ), array( $input, $label, $field ), $div );
 
 		// Add field to $fields array.
 		$fields[ $field ] = $div;
@@ -88,11 +86,20 @@ add_filter( 'comment_form_default_fields', 'bootswatch_comment_form_fields' );
  * Callback function for the `comment_form_defaults` filter hook
  *
  * @param  Array $defaults Defaults.
- * @return Array          Defaults modified
+ * @return Array           Defaults modified
  */
 function bootswatch_comment_form( $defaults ) {
-	$defaults['class_form'] = 'form-horizontal';
+	$defaults['class_form'] = 'form-horizontal well';
+	$defaults['comment_field'] = '
+		<div class="form-group comment-form-comment">
+			<label for="comment" class="col-sm-2 control-label">' . _x( 'Comment', 'noun' ) . ' <span class="required">*</span></label> 
+			<div class="col-sm-10">
+				<textarea class="form-control" id="comment" name="comment" cols="45" rows="8" required="required" aria-required="true"></textarea>
+			</div>
+		</div>'
+	;
+	$defaults['submit_field'] = '<div class="form-group"><div class="col-sm-offset-2 col-sm-10">%1$s %2$s</div></div>';
+	$defaults['class_submit'] = 'btn btn-primary btn-lg';
 	return $defaults;
 };
-
 add_filter( 'comment_form_defaults', 'bootswatch_comment_form' );
