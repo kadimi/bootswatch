be2c93acf5241025faa646da99f498d9d0cee7a5
Nabil Kadimi <nabil@kadimi.com>
2016-10-04 22:00:31 +0000

    Style comments

--- inc/extras/comment-walker.php
+++ inc/extras/comment-walker.php
@@ -21,18 +21,37 @@ class Walker_Comment_Bootswatch extends Walker_Comment {
 	 */
 	protected function html5_comment( $comment, $depth, $args ) {
 		ob_start();
-		parent::html5_comment( $comment, $depth, array_merge( $args, [ 'style' => 'div' ] ) );
-		echo str_replace( // WPCS XSS OK.
+		parent::html5_comment( $comment, $depth, $args );
+		$output = ob_get_clean();
+		$output = str_replace( // WPCS XSS OK.
 			[
 				'class="comment-body"',
 				'comment-reply-link',
+				'class="comment-metadata"',
+				'class="comment-edit-link"',
+				'class="reply"',
+				'<article id="div-comment-',
+				'</article>',
 			],
 			[
 				'class="comment-body well"',
 				'comment-reply-link btn btn-default btn-xs',
+				'class="comment-metadata small text-right"',
+				'class="comment-edit-link btn btn-danger btn-xs"',
+				'class="reply text-right"',
+				sprintf( '<div class="row"><div class="col-md-%1$d col-md-offset-%2$d"><article id="div-comment-'
+					, ( 13 - $depth )
+					, ( $depth -1 )
+				),
+				'</article></div></div>',
 			],
-			ob_get_clean()
+			$output
 		);
+
+		// Fix line exceeding link.
+		$output = preg_replace( "/\s*<\/time>\n\s+<\/a>/s", '</time></a>', $output );
+
+		echo $output;
 	}
 
 	/**
