c3ebc4c30ffd70cab77dfe161b6d82a4d2378319
Nabil Kadimi <nabil@kadimi.com>
2016-10-06 11:08:30 +0000

    Remove placeholder content from footer

--- footer.php
+++ footer.php
@@ -14,12 +14,7 @@
 <footer class="container">
 	<div class="row">
 		<div class="col-md-12"><hr></div>
-		<div class="col-md-8">
-			<nav>
-				<a href="#">Terms of Service</a> | <a href="#">Privacy</a>
-			</nav>
-		</div>
-		<div class="col-md-4">
+		<div class="col-md-4 col-md-offset-8">
 			<p class="muted pull-right small">
 				© <?php echo date( 'Y' ); // WPCS: XSS OK. ?> <?php echo esc_html( get_bloginfo( 'title' ) ); ?>.
 			</p>
