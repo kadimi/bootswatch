bf0eddf831cb53c61d349d3e144d90854d468eff
Nabil Kadimi <nabil@kadimi.com>
2017-03-27 02:18:24 +0100

    Run phpcbf

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -15,26 +15,26 @@ function bootswatch_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>' /* . '<time class="updated" datetime="%3$s">%4$s</time>' */;
-		}
+			}
 
 		$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() ),
 		esc_attr( get_the_modified_date( 'c' ) ),
 		esc_html( get_the_modified_date() )
-		);
+			);
 
-		$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'bootswatch' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-		);
+			$posted_on = sprintf(
+			esc_html_x( 'Posted on %s', 'post date', 'bootswatch' ),
+			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+			);
 
-		$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'bootswatch' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-		);
+			$byline = sprintf(
+			esc_html_x( 'by %s', 'post author', 'bootswatch' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			);
 
-		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+			echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
