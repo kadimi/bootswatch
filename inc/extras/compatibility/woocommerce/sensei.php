544c9c1b41a3aad0c3481d79f6f20b57d006c4c6
Nabil Kadimi <nabil@kadimi.com>
2017-03-27 02:17:24 +0100

    Add support for woocommerce/sensei

--- inc/extras/compatibility/woocommerce/sensei.php
+++ inc/extras/compatibility/woocommerce/sensei.php
@@ -4,19 +4,16 @@ global $woothemes_sensei;
 remove_action( 'sensei_before_main_content', [ $woothemes_sensei->frontend, 'sensei_output_content_wrapper' ] );
 remove_action( 'sensei_after_main_content', [ $woothemes_sensei->frontend, 'sensei_output_content_wrapper_end' ] );
 
-add_action('sensei_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('sensei_after_main_content', 'my_theme_wrapper_end', 10);
-
-function my_theme_wrapper_start() {
+add_action('sensei_before_main_content', function() {
 	?>
 	<div class="container">
 		<div class="row">
 			<div id="primary" class="<?php echo esc_attr( bootswatch_primary_classes() ); ?>">
 				<div id="content" role="main">
 	<?php
-}
+} );
 
-function my_theme_wrapper_end() {
+add_action('sensei_after_main_content', function () {
 	?>
 				</div>
 			</div>
@@ -25,4 +22,4 @@ function my_theme_wrapper_end() {
 	</div>
 	<?php
 	get_sidebar();
-}
+} );
