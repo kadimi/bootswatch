fd3af379a75c2b5acb39f56a5549bd1e063ffbcb
Nabil Kadimi <nabil@kadimi.com>
2017-07-08 08:35:44 +0100

    Format code

--- inc/extras/dependencies.php
+++ inc/extras/dependencies.php
@@ -12,18 +12,17 @@ require get_template_directory() . '/vendor/autoload.php';
 
 /**
  * Requires plugins with TGMPA:
+ *
+ * Example
+ *
+ * tgmpa( array(
+ * 	array(
+ * 		'name' => 'Titan Framework',
+ * 		'slug' => 'titan-framework',
+ * 		'required' => true,
+ * 	),
+ * ), array() );
  */
 function bootswatch_setup_dependencies() {
-	/**
-	 * Example
-	 *
-	tgmpa( array(
-		array(
-			'name' => 'Titan Framework',
-			'slug' => 'titan-framework',
-			'required' => true,
-		),
-	), array() );
-	 */
 }
 add_action( 'tgmpa_register', 'bootswatch_setup_dependencies' );
