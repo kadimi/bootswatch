13114954478b746015bc07474f7adef1ae0f7e25
Nabil Kadimi <nabil@kadimi.com>
2017-07-13 08:31:22 +0100

    Minor

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -44,7 +44,7 @@ function bootswatch_custom_header_setup() {
 		'default-image' => array(
 			'url'           => '%s/img/header.jpg',
 			'thumbnail_url' => '%s/img/header.jpg',
-			'description'   => __( 'Default Header Image', 'bootswatch_has' ),
+			'description'   => __( 'Default Header Image', 'bootswatch' ),
 		),
 	) );
 }
