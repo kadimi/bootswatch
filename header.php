8cc3502ac88734332e77db4af67107bdc560aae0
Nabil Kadimi <nabil@kadimi.com>
2017-07-13 16:29:20 +0100

    Remove unneded favicon code and unused custom header function

--- header.php
+++ header.php
@@ -16,9 +16,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php if ( has_site_icon() ) { ?>
-	<link rel="shortcut icon" href="<?php echo get_site_icon_url(); // XSS OK. ?>" />
-<?php } ?>
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -27,10 +27,6 @@ add_action( 'body_class', function( $body_classes ) {
 
 /**
  * Set up the WordPress core custom header feature.
- *
- * @uses bootswatch_header_style()
- * @uses bootswatch_admin_header_style()
- * @uses bootswatch_admin_header_image()
  */
 function bootswatch_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'bootswatch_custom_header_args', array(
@@ -49,93 +45,3 @@ function bootswatch_custom_header_setup() {
 	) );
 }
 add_action( 'after_setup_theme', 'bootswatch_custom_header_setup' );
-
-if ( ! function_exists( 'bootswatch_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog
- *
- * @see bootswatch_custom_header_setup().
- */
-function bootswatch_header_style() {
-		$header_text_color = get_header_textcolor();
-
-		// If no custom options for text are set, let's bail
-		// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value.
-		if ( HEADER_TEXTCOLOR === $header_text_color ) {
-			return;
-			}
-
-		// If we get this far, we have custom styles. Let's do this.
-		?>
-		<style type="text/css">
-		<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that.
-		else :
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo esc_attr( $header_text_color ); ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
-}
-endif;
-
-if ( ! function_exists( 'bootswatch_admin_header_style' ) ) :
-/**
- * Styles the header image displayed on the Appearance > Header admin panel.
- *
- * @see bootswatch_custom_header_setup().
- */
-function bootswatch_admin_header_style() {
-		?>
-		<style type="text/css">
-		.appearance_page_custom-header #headimg {
-			border: none;
-		}
-		#headimg h1,
-		#desc {
-		}
-		#headimg h1 {
-		}
-		#headimg h1 a {
-		}
-		#desc {
-		}
-		#headimg img {
-		}
-		</style>
-		<?php
-}
-endif;
-
-if ( ! function_exists( 'bootswatch_admin_header_image' ) ) :
-/**
- * Custom header image markup displayed on the Appearance > Header admin panel.
- *
- * @see bootswatch_custom_header_setup().
- */
-function bootswatch_admin_header_image() {
-		?>
-		<div id="headimg">
-		<h1 class="displaying-header-text">
-			<a id="name" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>" onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-		</h1>
-		<div class="displaying-header-text" id="desc" style="<?php echo esc_attr( 'color: #' . get_header_textcolor() ); ?>"><?php bloginfo( 'description' ); ?></div>
-		<?php if ( get_header_image() ) : ?>
-		<img src="<?php header_image(); ?>" alt="">
-		<?php endif; ?>
-		</div>
-		<?php
-}
-endif;
