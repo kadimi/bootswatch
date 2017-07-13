d1edb5bcf76bf6360676c08f414e1a5add6857ca
Nabil Kadimi <nabil@kadimi.com>
2017-07-13 16:47:09 +0100

    Add footer link

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 		<div class="col-md-12"><hr></div>
 		<div class="col-md-4 col-md-offset-8">
 			<p class="muted pull-right small">
-				&copy; <?php echo date( 'Y' ); // WPCS: XSS OK. ?> <?php echo esc_html( get_bloginfo( 'title' ) ); ?>.
+				&copy; <?php echo date( 'Y' ); // WPCS: XSS OK. ?> <?php echo esc_html( get_bloginfo( 'title' ) ); ?> &ndash; Designed by <a href='https://kadimi.com/' title="Nabil Kadimi - Independent WordPress Developer" rel="nofollow">Nabil</a>
 			</p>
 		</div>
 	</div>
