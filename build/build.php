0f7eab49ed7a1bf0c5cd518be156aaf6560ba553
Nabil Kadimi <nabil@kadimi.com>
2019-03-16 22:26:56 +0100

    Fix builder

--- build/build.php
+++ build/build.php
@@ -52,20 +52,20 @@ require 'build/class-bootswatchbuild.php';
 			sprintf( "/\"%s:[^\n]+\n/", 'Language-Team' )   => '',
 			sprintf( "/\"%s:[^\n]+\n/", 'Last-Translator' ) => '',
 			sprintf( "/\"%s:[^\n]+\n/", 'Plural-Forms' )    => '',
-			'/"PO-([a-z]+)-Date: .*/i'                      => '"PO-$1-Date: YEAR-MO-DA HO:MI+ZONE\n"',
+			'/"(PO|POT)-([a-z]+)-Date: .*/i'                => '"$1-$2-Date: YEAR-MO-DA HO:MI+ZONE\n"',
 		],
 		'languages/ar.po' => [
-			'/"PO-([a-z]+)-Date: .*/i' => '"PO-$1-Date: YEAR-MO-DA HO:MI+ZONE\n"',
-			'/charset=CHARSET/'        => 'charset=utf-8',
+			'/"(PO|POT)-([a-z]+)-Date: .*/i' => '"$1-$2-Date: YEAR-MO-DA HO:MI+ZONE\n"',
+			'/charset=CHARSET/'              => 'charset=utf-8',
 		],
 		'languages/fr.po' => [
-			'/"PO-([a-z]+)-Date: .*/i' => '"PO-$1-Date: YEAR-MO-DA HO:MI+ZONE\n"',
-			'/charset=CHARSET/'        => 'charset=utf-8',
+			'/"(PO|POT)-([a-z]+)-Date: .*/i' => '"$1-$2-Date: YEAR-MO-DA HO:MI+ZONE\n"',
+			'/charset=CHARSET/'              => 'charset=utf-8',
 		],
 	],
 	'vendor_ignored_patterns' => [
-		'extensions'   => '\.md$',
-		'cssjanus-0'   => '^cssjanus/cssjanus/[^/]+$',
-		'cssjanus-1'   => '^cssjanus/cssjanus/test/$',
+		'extensions' => '\.md$',
+		'cssjanus-0' => '^cssjanus/cssjanus/[^/]+$',
+		'cssjanus-1' => '^cssjanus/cssjanus/test/$',
 	],
 ] ) );
