f0e581903cec113a9f4536838121389b285d10e4
Nabil Kadimi <nabil@kadimi.com>
2020-10-06 03:53:39 +0100

    Lint

--- bootswatch-template-parts/components/header-menu.php
+++ bootswatch-template-parts/components/header-menu.php
@@ -18,8 +18,7 @@ if ( ! function_exists( 'bootswatch_header_menu_title_cb' ) ) {
 	function bootswatch_header_menu_title_cb( $title, $item, $args, $depth ) {
 		return 0 === $depth && in_array( 'menu-item-has-children', $item->classes )
 			? $title . ' <span class="caret"></span>'
-			: $title
-		;
+			: $title;
 	}
 }
 
@@ -31,14 +30,16 @@ add_filter( 'nav_menu_item_title', 'bootswatch_header_menu_title_cb', 10, 4 );
 /**
  * Display primary menu.
  */
-wp_nav_menu( array(
-	'theme_location' => 'primary',
-	'container' => false,
-	'menu_class' => 'nav navbar-nav' . ( ! bootswatch_has( 'search_form_in_header' ) ? ' navbar-right' : '' ),
-	'walker' => new Bootswatch_Nav_Walker,
-	'fallback_cb' => false,
-	'depth' => 2,
-) );
+wp_nav_menu(
+	array(
+		'theme_location' => 'primary',
+		'container'      => false,
+		'menu_class'     => 'nav navbar-nav' . ( ! bootswatch_has( 'search_form_in_header' ) ? ' navbar-right' : '' ),
+		'walker'         => new Bootswatch_Nav_Walker(),
+		'fallback_cb'    => false,
+		'depth'          => 2,
+	) 
+);
 
 /**
  * Hook callback so it doesn't run for other elements in the page.
