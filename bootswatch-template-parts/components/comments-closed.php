405084b1aec1e3f6e382907d0a61a8ebac87cce7
Nabil Kadimi <nabil@kadimi.com>
2020-10-06 03:50:27 +0100

    Lint

--- bootswatch-template-parts/components/comments-closed.php
+++ bootswatch-template-parts/components/comments-closed.php
@@ -5,8 +5,11 @@
  * @package Bootswatch
  */
 
-// If comments are closed and there are comments, let's leave a little note, shall we?
-if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-?>
+/**
+ * Add a note if the comments are closed and there are comments.
+ */
+if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) {
+	?>
 	<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'bootswatch' ); ?></p>
-<?php endif; ?>
+	<?php
+}
