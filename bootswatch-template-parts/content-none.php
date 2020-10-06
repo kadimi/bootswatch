df3ad0168342acfa91a6a906ec9eeb9bb0e0ae2c
Nabil Kadimi <nabil@kadimi.com>
2020-10-06 03:55:41 +0100

    Lint

--- bootswatch-template-parts/content-none.php
+++ bootswatch-template-parts/content-none.php
@@ -17,14 +17,22 @@
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php
+			<p>
+			<?php
 				// Translators: %s is a URL.
-				printf( wp_kses( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'bootswatch' ), [
-					'a' => [
-						'href' => [],
-					],
-				] ), esc_url( admin_url( 'post-new.php' ) ) );
-			?></p>
+				printf(
+					wp_kses(
+						__( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'bootswatch' ),
+						array(
+							'a' => array(
+								'href' => array(),
+							),
+						) 
+					),
+					esc_url( admin_url( 'post-new.php' ) ) 
+				);
+			?>
+			</p>
 
 		<?php elseif ( is_search() ) : ?>
 
