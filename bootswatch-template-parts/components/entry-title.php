8a8c08f574cfd785c6eccf46f96fc20e2615f78b
Nabil Kadimi <nabil@kadimi.com>
2020-10-06 03:52:50 +0100

    Lint

--- bootswatch-template-parts/components/entry-title.php
+++ bootswatch-template-parts/components/entry-title.php
@@ -8,14 +8,23 @@
 /**
  * Prepare classes.
  */
-$classes = [ 'entry-title' ];
+$classes = array( 'entry-title' );
 
 if ( is_singular() ) {
-	the_title( sprintf( '<h1 class="%s">', implode( ' ', $classes )
-	), '</h1>' );
+	the_title(
+		sprintf(
+			'<h1 class="%s">',
+			implode( ' ', $classes )
+		),
+		'</h1>' 
+	);
 } else {
-	the_title( sprintf( '<h2 class="%1$s"><a href="%2$s" rel="bookmark">'
-		, implode( ' ', $classes )
-		, esc_url( get_permalink() )
-	), '</a></h2>' );
+	the_title(
+		sprintf(
+			'<h2 class="%1$s"><a href="%2$s" rel="bookmark">',
+			implode( ' ', $classes ),
+			esc_url( get_permalink() )
+		),
+		'</a></h2>' 
+	);
 }
