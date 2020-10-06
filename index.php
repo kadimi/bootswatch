e2e6de1895ec04debed41f57d4f0af4cbb80adf5
Nabil Kadimi <nabil@kadimi.com>
2020-10-06 03:58:27 +0100

    Lint

--- index.php
+++ index.php
@@ -11,8 +11,12 @@ if ( function_exists( 'bootswatch_get_template_part' ) ) {
 } else {
 	// Translators: %1$s is the required PHP version, %2$s in the one available.
 	$message = sprintf( __( 'Bootswatch requires PHP %1$s or newer, you are using PHP %2$s.', 'bootswatch' ), BOOTSWATCH_MINIMAL_PHP_VERSION, phpversion() );
-	wp_die( esc_html( $message ), null, array(
-		'back_link' => true,
-	) );
+	wp_die(
+		esc_html( $message ),
+		null,
+		array(
+			'back_link' => true,
+		) 
+	);
 }
 do_action( 'bootswatch_shutdown' );
