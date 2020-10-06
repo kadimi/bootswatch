4b9a8cee77dd38f2d4843060271f039ea58c0673
Nabil Kadimi <nabil@kadimi.com>
2020-10-06 03:51:16 +0100

    Lint

--- bootswatch-template-parts/components/comments-list.php
+++ bootswatch-template-parts/components/comments-list.php
@@ -8,10 +8,12 @@
 ?>
 <div class="comment-list">
 	<?php
-		wp_list_comments( [
-			'style' => 'div',
-			'walker' => new Walker_Comment_Bootswatch(),
+	wp_list_comments(
+		array(
+			'style'      => 'div',
+			'walker'     => new Walker_Comment_Bootswatch(),
 			'short_ping' => true,
-		] );
+		) 
+	);
 	?>
 </div><!-- .comment-list -->
