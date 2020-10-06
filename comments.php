0474888f057737a53f73c2999c6908d92d9d382d
Nabil Kadimi <nabil@kadimi.com>
2020-10-06 03:59:49 +0100

    Lint

--- comments.php
+++ comments.php
@@ -17,27 +17,27 @@ if ( post_password_required() ) {
 /**
  * Wrap comment form title in <h2> if there are no comments
  */
-add_filter( 'bootswatch_comment_form_args', function( $args ) {
-	if ( ! have_comments() ) {
-		$args['title_reply_before'] = '<h2>';
-		$args['title_reply_after']  = '</h2>';
-	}
-	return $args;
-} );
+add_filter(
+	'bootswatch_comment_form_args',
+	function( $args ) {
+		if ( ! have_comments() ) {
+			$args['title_reply_before'] = '<h2>';
+			$args['title_reply_after']  = '</h2>';
+		}
+		return $args;
+	} 
+);
 
 ?>
 
-<div id="comments" class="comments-area"><?php
-
-	if ( have_comments() ) {
-
-		bootswatch_get_template_part( 'template-parts/components/comments', 'title' );
-		bootswatch_get_template_part( 'template-parts/components/comments', 'list' );
-		bootswatch_get_template_part( 'template-parts/components/comments', 'navigation' );
-	}
-
-	bootswatch_get_template_part( 'template-parts/components/comments', 'closed' );
-
-	comment_form( apply_filters( 'bootswatch_comment_form_args', [] ) );
-
-?></div>
+<div id="comments" class="comments-area">
+<?php
+if ( have_comments() ) {
+	bootswatch_get_template_part( 'template-parts/components/comments', 'title' );
+	bootswatch_get_template_part( 'template-parts/components/comments', 'list' );
+	bootswatch_get_template_part( 'template-parts/components/comments', 'navigation' );
+}
+bootswatch_get_template_part( 'template-parts/components/comments', 'closed' );
+comment_form( apply_filters( 'bootswatch_comment_form_args', array() ) );
+?>
+</div>
