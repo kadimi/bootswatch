879df6de821da25154c333327758f7233ba4b68d
Nabil Kadimi <nabil@kadimi.com>
2020-10-22 04:10:47 +0100

    Remove page-header class from blog homepage

--- inc/classes.php
+++ inc/classes.php
@@ -13,11 +13,8 @@
 function bootswatch_primary_classes() {
 	$classes = is_active_sidebar( 'sidebar' )
 		? 'col-md-8'
-		: 'col-md-12';
-
-	if ( is_home() ) {
-		$classes .= ' page-header';
-	}
+		: 'col-md-12'
+	;
 
 	$classes = apply_filters( 'bootswatch_primary_classes', $classes );
 
